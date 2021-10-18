648b9d50bda8327281d7e0f2eb09b4c40f9a05cd
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-10-18 19:20:31 +0300

    fix export state type

--- src/init/reducer.ts
+++ src/init/reducer.ts
@@ -3,7 +3,6 @@ import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({});
 
-type AppState = ReturnType<typeof rootReducer>;
+export type AppState = ReturnType<typeof rootReducer>;
 
 export { rootReducer };
-export type { AppState };
